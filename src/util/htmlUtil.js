ed209386b82b7cc62127e0be9ebda19a8091f4b5
wuchangming <wuchangmingnice@163.com>
2016-09-20 02:26:58 +0800

    hack html Content-Security-Policy

--- src/util/htmlUtil.js
+++ src/util/htmlUtil.js
@@ -5,8 +5,11 @@ var htmlUtil = exports;
 
 // inject script into html
 htmlUtil.injectScriptIntoHtml = function(html, script) {
-    html = html.replace(/(<\/head>)/i, function (match) {
+    html = html.replace(/(<\/head>)/ig, function (match) {
         return script + match;
+    });
+    html = html.replace(/Content-Security-Policy/ig, function (match) {
+        return 'hacky';
     });
     return html;
 }
