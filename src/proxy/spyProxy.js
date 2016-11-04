aadf6cb592071becad58483502a510b13fdbf3a3
wuchangming <wuchangmingnice@163.com>
2016-11-04 12:54:17 +0800

    weinre console.log fails when given null value

--- src/proxy/spyProxy.js
+++ src/proxy/spyProxy.js
@@ -14,6 +14,7 @@ const jschardet = require('jschardet');
 const domain = require('domain');
 const childProcess = require('child_process');
 
+var targetScript = fs.readFileSync(path.resolve(__dirname, '../../template/target-script-min.js'));
 
 var d = domain.create();
 d.on('error', function (err) {
@@ -71,8 +72,16 @@ module.exports = {
                         next();
                         return;
                     }
-                    // console.log(req.path);
                     if (rOptions.headers.host === config.SPY_WEINRE_DOMAIN) {
+
+                        // fixed  console.log(null) or console.log(undefined) occur error bug!
+                        if (rPath === '/target/target-script-min.js') {
+                            res.setHeader('Content-Type', 'application/javascript');
+                            res.end(targetScript.toString());
+                            next();
+                            return;
+                        }
+
                         rOptions.protocol = 'http:';
                         rOptions.hostname = '127.0.0.1';
                         rOptions.port = weinrePort;
