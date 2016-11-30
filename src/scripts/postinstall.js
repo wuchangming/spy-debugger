15922e968cc27c8243adca88c88ea753506cf6cf
wuchangming <wuchangmingnice@163.com>
2016-11-30 12:02:01 +0800

    fixed nodeV7 crash

--- src/scripts/postinstall.js
+++ src/scripts/postinstall.js
@@ -3,6 +3,9 @@ const path = require('path');
 try {
     const newTextPromptJs = fs.readFileSync(path.join(__dirname, '../../template/TextPrompt.js'))
     fs.writeFileSync(path.join(__dirname, '../../node_modules/weinre/web/client/TextPrompt.js'), newTextPromptJs)
+
+    const utilsJs = fs.readFileSync(path.join(__dirname, '../../template/utils.js'))
+    fs.writeFileSync(path.join(__dirname, '../../node_modules/weinre/lib/utils.js'), utilsJs)
 } catch (e) {
 //
 }
