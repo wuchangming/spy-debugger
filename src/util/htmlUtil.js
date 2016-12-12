8c15977f49e0ddf0eedf7c7369e48521e3e21cd7
wuchangming <wuchangmingnice@163.com>
2016-12-12 13:07:25 +0800

    optimize <head> tag regexp

--- src/util/htmlUtil.js
+++ src/util/htmlUtil.js
@@ -5,7 +5,7 @@ var htmlUtil = exports;
 
 // inject script into html
 htmlUtil.injectScriptIntoHtml = function(html, script) {
-    html = html.replace(/<head>/ig, function (match) {
+    html = html.replace(/<head>|<head\s[^<]*>/gi, function (match) {
         return match + script;
     });
     html = html.replace(/Content-Security-Policy/ig, function (match) {
