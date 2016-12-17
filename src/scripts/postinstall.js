3b23bf5ec05fcc11e3ce23573e2e2b41f74c2caf
wuchangming <wuchangmingnice@163.com>
2016-12-17 20:32:11 +0800

    modify style

--- src/scripts/postinstall.js
+++ src/scripts/postinstall.js
@@ -6,6 +6,9 @@ try {
 
     const utilsJs = fs.readFileSync(path.join(__dirname, '../../template/utils.js'))
     fs.writeFileSync(path.join(__dirname, '../../node_modules/weinre/lib/utils.js'), utilsJs)
+
+    const anyproxyIndexHtml = fs.readFileSync(path.join(__dirname, '../../template/anyproxy_index.html'))
+    fs.writeFileSync(path.join(__dirname, '../../node_modules/anyproxy/web/index.html'), anyproxyIndexHtml)
 } catch (e) {
 //
 }
