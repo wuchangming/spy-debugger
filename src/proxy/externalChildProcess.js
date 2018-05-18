f388643d13dc82a510e8cb3e717eb6937a8a1135
wuchangming <wuchangmingnice@163.com>
2018-05-18 15:00:06 +0800

    update anyproxy

--- src/proxy/externalChildProcess.js
+++ src/proxy/externalChildProcess.js
@@ -1,11 +1,9 @@
 require('babel-polyfill')
 const http = require('http')
-const default_rule = require('anyproxy/lib/rule_default')
-const proxy = require('anyproxy')
+const AnyProxy = require('AnyProxy')
 const fs = require('fs')
 const path = require('path')
-const colors = require('colors');
-
+const colors = require('colors')
 
 let port, webPort, socketPort
 
@@ -13,7 +11,7 @@ var tempServerPromise = () => {
     return new Promise((resolve, reject) => {
         let tempServer = new http.Server()
         tempServer.listen(() => {
-            let unBoundedPort = tempServer.address().port;
+            let unBoundedPort = tempServer.address().port
             tempServer.close(() => {
                 resolve(unBoundedPort)
             })
@@ -23,40 +21,47 @@ var tempServerPromise = () => {
 
 let createAnyProxy = () => {
     var options = {
-        type          : "http",
         port,
-        hostname      : "127.0.0.1",
-        rule          : Object.assign(default_rule, {
-            shouldInterceptHttpsReq: () => true
-        }),
-        webPort,  // optional, port for web interface
-        socketPort,
-        silent        : true //optional, do not print anything into terminal. do not set it when you are still debugging.
+        forceProxyHttps: true,
+        webInterface: {
+            enable: true,
+            webPort
+        },
+        dangerouslyIgnoreUnauthorized: true,
+        silent: true //optional, do not print anything into terminal. do not set it when you are still debugging.
     }
-    new proxy.proxyServer(options)
+    new AnyProxy.ProxyServer(options).start()
 }
 
-process.on('message', ({type, ports}) => {
+process.on('message', ({ type, ports }) => {
     if (type === 'start') {
-        if(!proxy.isRootCAFileExists()) {
+        if (!AnyProxy.utils.certMgr.isRootCAFileExists()) {
             var userHome = process.env.HOME || process.env.USERPROFILE
-            var certDir = path.join(userHome, "/.anyproxy_certs/")
-            if(!fs.existsSync(certDir)){
-                try{
+            var certDir = path.join(userHome, '/.anyproxy/certificates')
+            if (!fs.existsSync(certDir)) {
+                try {
                     fs.mkdirSync(certDir)
-                }catch(e){
+                } catch (e) {
                     console.error('fail to create certDir at:' + certDir)
                 }
             }
             var mitmCrt = path.resolve(userHome, './node-mitmproxy/node-mitmproxy.ca.crt')
             var mitmKey = path.resolve(userHome, './node-mitmproxy/node-mitmproxy.ca.key.pem')
 
-            fs.createReadStream(mitmCrt).pipe(fs.createWriteStream(path.join(certDir, './rootCA.crt')))
-            fs.createReadStream(mitmKey).pipe(fs.createWriteStream(path.join(certDir, './rootCA.key')))
+            fs
+                .createReadStream(mitmCrt)
+                .pipe(fs.createWriteStream(path.join(certDir, './rootCA.crt')))
+            fs
+                .createReadStream(mitmKey)
+                .pipe(fs.createWriteStream(path.join(certDir, './rootCA.key')))
         }
 
-        (async () => {
-            let ports = await Promise.all([tempServerPromise(), tempServerPromise(), tempServerPromise()])
+        ;(async () => {
+            let ports = await Promise.all([
+                tempServerPromise(),
+                tempServerPromise(),
+                tempServerPromise()
+            ])
             port = ports[0]
             webPort = ports[1]
             socketPort = ports[2]
@@ -68,13 +73,12 @@ process.on('message', ({type, ports}) => {
                 socketPort
             })
         })()
-
     } else if (type === 'restart') {
         port = ports.port
         webPort = ports.webPort
         socketPort = ports.socketPort
 
         createAnyProxy()
-        console.log(colors.green('重启成功！请手动刷新浏览器'));
+        console.log(colors.green('重启成功！请手动刷新浏览器'))
     }
 })
--- src/scripts/postinstall.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-try {
-    const anyproxyIndexHtml = fs.readFileSync(path.join(__dirname, '../../template/anyproxy_index.html'))
-    fs.writeFileSync(path.join(__dirname, '../../node_modules/anyproxy/web/index.html'), anyproxyIndexHtml)
-} catch (e) {
-//
-}
